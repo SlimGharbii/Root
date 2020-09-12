815da1b9c9c5cf3e4de197dbe4032ebf9edb0e08
Slim Gharbi <59753023+bamboutcha@users.noreply.github.com>
2020-09-12 16:07:34 +0200

    Add files via upload

--- app.module.ts
+++ app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ComponenteComponent } from './componente/componente.component';
 import { ComponenteesComponent } from './componentees/componentees.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ComponenteComponent,
-    ComponenteesComponent
+    ComponenteesComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
